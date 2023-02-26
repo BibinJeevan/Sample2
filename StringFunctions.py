b664318bb1e99ae8c11ec1889f25ea0a97ddf6eb
BibinJeevan <125947048+BibinJeevan@users.noreply.github.com>
2023-02-27 00:05:06 +0530

    String Functions

---
+++ StringFunctions.py
@@ -0,0 +1,10 @@
+a=str(input("Enter the string"))
+print("capitalized:",a.capitalize())
+substr1="od"
+substr2="ing"
+substr3="o"
+print("count=",a.count(substr3))
+print("find1=",a.find(substr1))
+print("find2=",a.find(substr2))
+print("Lowercase",a.islower())
+print("Uppercase=",a.isupper())
